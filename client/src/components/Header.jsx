cf98502130a539f6be2f18f6b9dc4cf44789a2ab
Brian-Ambani <brainambani1@gmail.com>
2024-02-13 15:13:35 +0300

    updated the signin button to outline

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -35,7 +35,9 @@ export default function Header() {
             <FaMoon />
           </Button>
           <Link to="/sign-in">
-            <Button gradientDuoTone="purpleToBlue">Sign in</Button>
+            <Button gradientDuoTone="purpleToBlue" outline>
+              Sign in
+            </Button>
           </Link>
           <Navbar.Toggle />
         </div>
